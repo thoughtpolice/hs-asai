12451214b8aa799e487c379b4099f4912531b3a2
Austin Seipp <mad.one@gmail.com>
2012-12-31 05:12:06 -0600

    Two trivial benchmarks.
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- bench/Bench1.hs
+++ bench/Bench1.hs
@@ -1,8 +1,20 @@
 module Main
        ( main -- :: IO ()
        ) where
-import Control.Monad.Cont.Delimited ()
-import Criterion.Main (defaultMain)
+import Control.Monad.Cont.Delimited
+import Criterion.Main (defaultMain, bench, nf)
 
 main :: IO ()
-main = defaultMain []
+main = defaultMain
+       [ -- Simple answer-type modification
+         bench "ans1" $ nf id $ run $ reset
+         (shift (\_ -> ret "hello") !>>= \r ->
+           ret (r + 1 :: Int))
+
+         -- Capturing the continuation
+       , bench "ans2" $ nf id $ run $
+         (reset ( shift ret !>>= \x ->
+                  ret (x + 1)
+                ) !>>= \r ->
+         ret (r (5 :: Int)))
+       ]
