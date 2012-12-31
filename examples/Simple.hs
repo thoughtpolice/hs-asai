5c52cebe5a4f3e9c07e0ddd06f6cfbdd8c299fed
Austin Seipp <mad.one@gmail.com>
2012-12-31 11:21:03 -0600

    Type signatures
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- examples/Simple.hs
+++ examples/Simple.hs
@@ -18,12 +18,14 @@ io1 = do
 
 -- Simple test of answer type modification
 
+ans1 :: String
 ans1 = runDelim $ reset $ do
   r <- shift (\_ -> return "hello")
   return (r + 1)
 -- This is equivalent to the OchaCaml term:
 --   reset (fun () -> 1 + shift (fun _ -> "hello")) ;;
 
+ans2 :: Int
 ans2 = runDelim $ do
   r <- reset $ do
     x <- shift return
@@ -33,18 +35,21 @@ ans2 = runDelim $ do
 
 -- Append example, with monadic syntax
 
+appnd :: Monad' m => [a] -> Delim b ([a] -> m a' a' b) [a]
 appnd []       = shift (\k -> return (return . k))
 appnd (a:rest) = do
   r <- appnd rest
   return (a:r)
 
+testAppnd :: [Int]
 testAppnd = runDelim $ do
   f <- reset (appnd [1,2,3])
   f [4,5,6] -- [1,2,3,4,5,6]
 
 -- Visitors/cursors, with monadic syntax
 
-visit [] = shift (\h -> return [])
+visit :: [a] -> Delim b [b] [a]
+visit []       = shift (\_ -> return [])
 visit (a:rest) = do
   r <- shift (\k -> do
                     b <- return (k [])
@@ -53,5 +58,6 @@ visit (a:rest) = do
   return (a:r)
 
 
+testVisit :: [[Int]]
 testVisit = runDelim $ reset (visit [1,2,3])
 -- [[1],[1,2],[1,2,3]]
