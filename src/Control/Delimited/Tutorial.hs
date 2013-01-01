329b392d643a88561d49f3556f61bfd3957f10bd
Austin Seipp <mad.one@gmail.com>
2013-01-01 03:58:43 -0600

    Tutorial: write intro. Shim out remainder.
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- src/Control/Delimited/Tutorial.hs
+++ src/Control/Delimited/Tutorial.hs
@@ -17,6 +17,15 @@ module Control.Delimited.Tutorial
        ( -- * Introduction
          -- $intro
 
+         -- * Delimited continuations
+         -- $delimcc
+
+         -- ** Part 1
+         -- $pt1
+
+         -- ** Part 2
+         -- $pt2
+
          -- * Using @do@-notation
          -- $donotation
 
@@ -40,9 +49,57 @@ f =<< m  = m !>>= f
 
 {- $intro
 
+Continuations are a well known abstraction for 'picking up where you
+left off.' When you use @call/cc@ traditionally in something like
+scheme, you pass it a function @f@ which receives a function @k@, and
+when @k@ is invoked you 'call the continuation' and return where
+@call/cc@ left off. In this sense, @k@ is a reification of the state
+you were in when you invoked @f@.
+
+But @call/cc@ is, as some say, 'overrated.' For one, it is not
+possible to return a value from the continuation; you merely pick up
+where you left off. By fixing this we get delimited continuations: an
+abstraction that allows us to slice up a continuation and compose
+them. But at this point, we suffer in a statically typed language due
+to the fact that the type of the continuation is fixed, and cannot
+vary.
+
+In the same way that continuations form a monad, so do delimited
+continuations in their composition. This package provides a delimited
+continuation monad which implements truly polymorphic @shift@ and
+@reset@ operators, via /answer type polymorphism/, which allows the
+results of a delimited computation to vary.
+
+This implementation (using parameterized monads) was first implemented
+as "Genuine shift/reset in Haskell" by Oleg Kiselyov [1]. It directly
+implements the typing rules of K. Asai's lambda/shift language [2],
+featuring answer type polymorphism.
+
+A general tutorial on delimited continuations in OchaCaml, Haskell and
+Caml from Asai/Kiselyov is available [3].
+
+-}
+
+{- $delimcc
+
 Lorem ipsum...
 
->>> runDelim $ reset $ (shift1 (\_ -> return "hello") >>= \r -> return (r + 1))
+-}
+
+{- $pt1
+
+Lorem ipsum...
+
+>>> runDelim $ reset $ shift1 (\_ -> ret "hello") !>>= \r -> ret (r + 1)
+"hello"
+
+-}
+
+{- $pt2
+
+Lorem ipsum...
+
+>>> runDelim $ reset $ shift1 (\_ -> return "hello") >>= \r -> return (r + 1)
 "hello"
 
 -}
@@ -104,13 +161,13 @@ implementation by Kiselyov is Haskell98. You do not need to enable
 
 {- $refs
 
-  * /Polymorphic Delimited Continuations/, by Asai, Kameyama in /APLAS '07/:
-    <http://logic.cs.tsukuba.ac.jp/~kam/paper/aplas07.pdf>
+  1. /Genuine shift\reset in Haskell98/, by Kiselyov, on /haskell-cafe/:
+     <http://okmij.org/ftp/continuations/implementations.html#genuine-shift>
 
-  * /Genuine shift\reset in Haskell98/, by Kiselyov, on /haskell-cafe/:
-    <http://okmij.org/ftp/continuations/implementations.html#genuine-shift>
+  2. /Polymorphic Delimited Continuations/, by Asai, Kameyama in /APLAS '07/:
+     <http://logic.cs.tsukuba.ac.jp/~kam/paper/aplas07.pdf>
 
-  * /Introduction to programming with shift and reset/, by Kiselyov, Asai, in /CW2011/:
-    <http://okmij.org/ftp/continuations/index.html#tutorial>
+  3. /Introduction to programming with shift and reset/, by Kiselyov, Asai, in /CW2011/:
+      <http://okmij.org/ftp/continuations/index.html#tutorial>
 
 -}
