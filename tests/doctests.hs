068aebd23aab39578eae360222e0193117ef3fa4
Austin Seipp <mad.one@gmail.com>
2013-01-03 01:22:19 -0600

    Specify missing modules in doctests.
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -7,4 +7,6 @@ main :: IO ()
 main = doctest [ "-isrc"
                , "src/Control/Delimited.hs"
                , "src/Control/Delimited/Tutorial.hs"
+               , "src/Control/Indexed/Monad.hs"
+               , "src/Control/Indexed/Prelude.hs"
                ]
