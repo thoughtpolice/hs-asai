625e3a7d3597a49efad900ebd3808d957d4f5bb3
Austin Seipp <mad.one@gmail.com>
2013-01-06 01:38:27 -0600

    Make Printf example slightly less verbose.
    Signed-off-by: Austin Seipp <mad.one@gmail.com>

--- examples/Printf.hs
+++ examples/Printf.hs
@@ -4,7 +4,9 @@ module Printf where
 import Control.Indexed.Prelude
 import Control.Delimited
 
+int :: Int -> String
 int x = show x
+str :: String -> String
 str x = x
 
 f $$ x = f !>>= ($ x)
@@ -14,15 +16,12 @@ e1 ^$ e2 = liftIxM2 (++) e1 e2
 
 fmt to = shift2 (\k -> return (k . to))
 sprintf p = reset p
+run = runDelim
 
-stest1 = runDelim $
-  sprintf (ret "Hello world")
+stest1 = run $ sprintf (ret "Hello world")
 
-stest2 = runDelim $
-  sprintf (fmt int) $$ 3
+stest2 = run $ sprintf (fmt int) $$ 1
 
-stest3 = runDelim $
-  sprintf (ret "goodbye " ^$ fmt str ^$ ret "!") $$ "world"
+stest3 = run $ sprintf (ret "goodbye " ^$ fmt str ^$ ret "!") $$ "world"
 
-stest4 = runDelim $
-  sprintf (fmt str ^$ ret " = " ^$ fmt int) $$ "x" $$ 3
+stest4 = run $ sprintf (fmt str ^$ ret " = " ^$ fmt int) $$ "x" $$ 3
